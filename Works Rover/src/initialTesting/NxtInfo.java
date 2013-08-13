f66bca783f69bd611d90fae1b5b2917660353bf4
Eric Miller <eric@legoaces.org>
2013-08-12 23:28:09 -0400

    Added first fields

--- Works Rover/src/initialTesting/NxtInfo.java
+++ Works Rover/src/initialTesting/NxtInfo.java
@@ -1,5 +1,12 @@
 package initialTesting;
 
 public class NxtInfo {
-
+	public int comPort;
+	public String brickName;
+	public String friendlyName;
+	
+	public NxtInfo(int comPort, String name){
+		this.comPort = comPort;
+		this.brickName = this.friendlyName = name;
+	}
 }
