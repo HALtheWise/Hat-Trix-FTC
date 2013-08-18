494db9155beae19bfae27afbc665edf68774fd66
Eric Miller <eric@legoaces.org>
2013-08-18 15:15:26 -0400

    First GUI made

--- Works Rover/src/initialTesting/BasicGUI.java
+++ Works Rover/src/initialTesting/BasicGUI.java
@@ -3,6 +3,23 @@ package initialTesting;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BasicGUI {
 
@@ -36,8 +53,31 @@ public class BasicGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBackground(Color.WHITE);
+		frame.setBounds(100, 100, 509, 375);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"NXT #1", "NXT #2"}));
+		frame.getContentPane().add(comboBox);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel lblName = new JLabel("Name:");
+		panel.add(lblName);
+		
+		JLabel lblBtId = new JLabel("BT ID:");
+		panel.add(lblBtId);
+		
+		JButton btnScanForNxts = new JButton("Connect to NXT");
+		btnScanForNxts.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frame.getContentPane().add(btnScanForNxts);
 	}
 
 }
