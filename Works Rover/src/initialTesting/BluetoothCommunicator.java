8c22f0d2955136e76f91fe4480c69f1be63c959e
Eric Miller <eric@legoaces.org>
2013-08-25 11:25:34 -0400

    merged

--- Works Rover/src/initialTesting/BluetoothCommunicator.java
+++ Works Rover/src/initialTesting/BluetoothCommunicator.java
@@ -2,8 +2,6 @@ package initialTesting;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 
 import javax.bluetooth.*;
@@ -27,15 +25,16 @@ public class BluetoothCommunicator {
 	public static void main(String[] args){
 		BluetoothCommunicator bc = new BluetoothCommunicator("0016530082A7");
 		bc.connect(); 
-		System.out.println("connected");
-		try{System.in.read();}catch(Exception e){};
-		System.out.println("sending");
-		
-		byte[] data = "hello".getBytes();
-		sanitize(data);
-		bc.sendData(data);
-		
-		System.out.println("sent");
+		while(true){
+			System.out.println("connected. press enter to send message");
+			try{System.in.read();}catch(Exception e){};
+			System.out.println("sending");
+
+			byte[] data = "hello".getBytes();
+			bc.sendNXTMessage(data);
+			System.out.println("sent");
+			if(false) break;
+		}
 		bc.close();
 	}
 	
@@ -62,6 +61,23 @@ public class BluetoothCommunicator {
 		try {
 			out.write(data);
 			out.write(END_OF_MESSAGE);
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendNXTMessage(byte[] data){
+		byte[] dataToSend = new byte[data.length + 4];
+		dataToSend[0] = (byte) 0x80; //Command without response expected
+		dataToSend[1] = (byte) 0x09; //Direct Message
+		dataToSend[2] = (byte) 0x00; //Mailbox #1
+		dataToSend[3] = (byte) data.length; //Length of message
+		for (int i = 0; i<data.length; i++){
+			dataToSend[i+4] = data[i];
+		}
+		try {
+			out.write(dataToSend);
 			out.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
