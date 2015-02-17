7dfbb2266a31af34754f06e2ae662ccb6bb68cba
Eric Miller <eric@legoaces.org>
2015-02-17 12:24:50 -0500

    Added seperate Field Positions file

---
+++ Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
@@ -0,0 +1,6 @@
+
+FieldPos GPS_floorStartingPosition = {22.9, 213.0, degreesToRadians(0)};
+
+
+CenterRelativePos GPS_prepareForCenterDump = {-150, 0};
+CenterRelativePos GPS_centerDumpPosition = {-120, 0};
