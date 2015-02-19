23763fc1500ebafb84436be50233cb2552d4e988
Eric Miller <eric@legoaces.org>
2015-02-19 11:25:04 -0500

    Tweaked all dump positions

--- Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
+++ Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
@@ -22,9 +22,9 @@ void initPositions(){
 	GPS_navPoint1.x = 271; GPS_navPoint1.y = 255; GPS_navPoint1.theta = 0;
 
 	GPS_prepareForCenterDump.x = -99; GPS_prepareForCenterDump.y = 0;
-	GPS_centerDumpPosition1.x = -66; GPS_centerDumpPosition1.y = 7;
-	GPS_centerDumpPosition2.x = -63; GPS_centerDumpPosition2.y = 7;
-	GPS_centerDumpPosition3.x = -66; GPS_centerDumpPosition3.y = 3;
+	GPS_centerDumpPosition1.x = -65; GPS_centerDumpPosition1.y = 8;
+	GPS_centerDumpPosition2.x = -62; GPS_centerDumpPosition2.y = 8;
+	GPS_centerDumpPosition3.x = -65; GPS_centerDumpPosition3.y = 4;
 	GPS_prepareForKickstand.x = -85; GPS_prepareForKickstand.y = -35;
 	GPS_hitKickstand.x = 10; GPS_hitKickstand.y = -35;
 }
