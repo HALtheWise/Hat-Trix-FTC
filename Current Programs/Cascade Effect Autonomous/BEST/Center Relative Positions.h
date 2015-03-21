e6f6f75d1ee6fb707258aca73118da27b28abd45
Eric Miller <eric@legoaces.org>
2015-03-21 10:13:43 -0400

    Not yet working, leaving for Flute

--- Current Programs/Cascade Effect Autonomous/BEST/Center Relative Positions.h
+++ Current Programs/Cascade Effect Autonomous/BEST/Center Relative Positions.h
@@ -2,51 +2,67 @@
 typedef struct {
 	float x; //(0,0) is at the very center of the field. Positive x extends away from our color center goal.
 	float y; //Positive y extends away from our color kickstand
-} CenterRelativePos;
+	float theta;
+} RelativePos;
 
 int centerPos = -1;
+
 
 float getCenterAngle(){
 	if (centerPos == 1){
 		return degreesToRadians(-90);
-		}else if (centerPos == 2){
+	}else if (centerPos == 2){
 		return degreesToRadians(-45);
-		}else if (centerPos == 1){
+	}else if (centerPos == 1){
 		return 0;
-		}else{
+	}else{
 		writeDebugStreamLine("Warning: center position not detected yet!");
 		return 0;
 	}
 }
 
-void translate(CenterRelativePos input, FieldPos *result){
-	writeDebugStreamLine("Input was %d, %d", input.x, input.y);
+void getCenterFieldPos(FieldPos *result){
 	const float FIELD_SIZE = 365.76;
-	float angle = getCenterAngle(); //Heading of center structure with respect to field coords.
-	result->x = FIELD_SIZE/2;
-	result->y = FIELD_SIZE/2;
-	result->theta = angle;
+	float angle = getCenterAngle();
+	result.x = FIELD_SIZE/2;
+	result.y = FIELD_SIZE/2;
+	result.theta = angle;
+}
 
-	result->x = result->x + input.x * cos(angle) - input.y * sin(angle);
-	result->y = result->y + input.y * cos(angle) + input.x * sin(angle);
+void add(FieldPos base, RelativePos relative, FieldPos *result){
+	result.x = base.x;
+	result.y = base.y;
+	result.theta = base.theta;
+
+	result->x = result->x + relative.x * cos(result.theta) - relative.y * sin(result.theta);
+	result->y = result->y + relative.y * cos(result.theta) + relative.x * sin(result.theta);
+	result->theta = result->theta + relative->theta;
+}
+
+void translate(RelativePos input, FieldPos *result){
+	writeDebugStreamLine("Input was %d, %d", input.x, input.y);
+	
+	FieldPos centerAbsPos;
+	getCenterFieldPos(centerAbsPos);
+	add(centerAbsPos, input, result);
 
 	writeDebugStreamLine("Output was %d, %d", result->x, result->y);
 }
 
 
-void turnAndMoveTo (CenterRelativePos target, int power, DrivingDirection forward = Forward){
+void turnAndMoveTo (RelativePos target, int power, DrivingDirection forward = Forward){
 	FieldPos target2;
 	translate(target, target2);
 	turnAndMoveTo(target2, power, forward);
 }
 
-void turnTo (CenterRelativePos target, int power, DrivingDirection forward = Forward){
+void turnTo (RelativePos target, int power, DrivingDirection forward = Forward){
 	FieldPos target2;
 	translate(target, target2);
 	turnTo(target2, power, forward);
 }
 
-void moveTo (CenterRelativePos target, int power, DrivingDirection forward = Forward){
+void moveTo (RelativePos target, int power, DrivingDirection forward = Forward){
 	FieldPos target2;
 	translate(target, target2);
 	moveTo(target2, power, forward);
