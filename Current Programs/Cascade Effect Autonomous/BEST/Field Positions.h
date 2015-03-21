8b0c36d164ccee35b82f29ef063989ce9e9730e7
Eric Miller <eric@legoaces.org>
2015-03-21 17:24:07 -0400

    Removed dead code

--- Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
+++ Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
@@ -1,7 +1,4 @@
-
-FieldPos GPS_floorStartingPosition;
 FieldPos GPS_floorStartingPositionUS;
-FieldPos GPS_awayFromWall;
 FieldPos GPS_awayFromWallUS;
 FieldPos GPS_mediumGoalPosition;
 FieldPos GPS_navPoint1;
@@ -29,29 +26,20 @@ void set(RelativePos *pos, float x, float y, float theta = 0){
 void initPositions(){
 	set(GPS_floorStartingPositionUS, 10, 198, degreesToRadians(180));
 	set(GPS_awayFromWallUS, 50, 195);
-	//GPS_floorStartingPositionUS.x = 10; GPS_floorStartingPositionUS.y = 198.0; GPS_floorStartingPositionUS.theta = degreesToRadians(180);
-	//GPS_awayFromWallUS.x = 50; GPS_awayFromWallUS.y = 195.0; GPS_awayFromWallUS.theta = 0;
 
 	set(GPS_mediumGoalPosition, 271, 20);
 	set(GPS_navPoint1, 271, 255);
-	// GPS_mediumGoalPosition.x = 271; GPS_mediumGoalPosition.y = 20; GPS_mediumGoalPosition.theta = 0;
-	// GPS_navPoint1.x = 271; GPS_navPoint1.y = 255; GPS_navPoint1.theta = 0;
 
 	set(GPS_defendPoint, 80, 320);
-	// GPS_defendPoint.x = 80; GPS_defendPoint.y = 320; GPS_defendPoint.theta = 0;
 
 	set(GPS_prepareForCenterDump, -99, 0);
 	set(GPS_centerDumpPosition1, -40, 8);
 	set(GPS_centerDumpPosition2, -36, 8);
 	set(GPS_centerDumpPosition3, -38, 4);
-	// GPS_prepareForCenterDump.x = -99; GPS_prepareForCenterDump.y = 0;
-	// GPS_centerDumpPosition1.x = -65; GPS_centerDumpPosition1.y = 8;
-	// GPS_centerDumpPosition2.x = -61; GPS_centerDumpPosition2.y = 8;
-	// GPS_centerDumpPosition3.x = -63; GPS_centerDumpPosition3.y = 4;
+
 	set(GPS_prepareForKickstand, -85, -35);
 	set(GPS_hitKickstand, 10, -35);
-	// GPS_prepareForKickstand.x = -85; GPS_prepareForKickstand.y = -35;
-	// GPS_hitKickstand.x = 10; GPS_hitKickstand.y = -35;
+
 
 	set(ROBOT_nullRelativePos, 0, 0);
 	set(ROBOT_dumpRelativePos, -25, 0, 0);
