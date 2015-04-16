83c6fc9ba6dd9a1e96e3f9a0ebf5fd4433f77c18
Eric Miller <eric@legoaces.org>
2015-04-16 16:14:06 -0400

    Functioning down ramp and goal grab with US measurement along the way.

--- Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
+++ Current Programs/Cascade Effect Autonomous/BEST/Field Positions.h
@@ -15,8 +15,6 @@ RelativePos GPS_centerDumpPosition2;
 RelativePos GPS_centerDumpPosition3;
 RelativePos GPS_prepareForKickstand;
 RelativePos GPS_hitKickstand;
-RelativePos GPS_prepareForKickstandReverse;
-RelativePos GPS_hitKickstandReverse;
 
 RelativePos ROBOT_dumpRelativePos; //Warning: these are robot-relative, not field-relative
 RelativePos ROBOT_nullRelativePos;
@@ -28,6 +26,8 @@ void set(FieldPos *pos, float x, float y, float theta = 0){
 void set(RelativePos *pos, float x, float y, float theta = 0){
 	pos->x = x; pos->y =  y; pos->theta = theta;
 }
+
+void initPositionsRamp();
 
 void initPositions(){
 	set(GPS_floorStartingPositionUS, 10, 198, degreesToRadians(180));
@@ -50,10 +50,40 @@ void initPositions(){
 
 	set(GPS_prepareForKickstand, -85, -35);
 	set(GPS_hitKickstand, 10, -35);
-	set(GPS_prepareForKickstandReverse, 65, -35);
-	set(GPS_hitKickstandReverse, -10, -35);
 
 
 	set(ROBOT_nullRelativePos, 0, 0);
 	set(ROBOT_dumpRelativePos, -25, 0, 0);
+	initPositionsRamp();
+}
+
+FieldPos GPS_rampStartingPosition;
+FieldPos GPS_offRamp;
+FieldPos GPS_mediumGoalPositionRamp;
+FieldPos GPS_almostMediumGoalPositionRamp;
+
+RelativePos GPS_prepareForKickstandReverse;
+RelativePos GPS_hitKickstandReverse;
+RelativePos GPS_defendPointRamp;
+
+
+void initPositionsRamp(){
+	set(GPS_rampStartingPosition,
+	22.9 - 12.8, 	//9in, defined from robot away from wall, minus 5inches due to the slope of the ramp and trigonometry.
+	30.2, 		//12in, on ramp half way from wall.
+	degreesToRadians(180));
+
+	set(GPS_offRamp, 165, GPS_rampStartingPosition.y);
+
+	set(GPS_mediumGoalPositionRamp, 300, 30);
+	set(GPS_almostMediumGoalPositionRamp, GPS_mediumGoalPositionRamp.x - 25, GPS_mediumGoalPositionRamp.y);
+
+	set(GPS_navPoint1, 291, 255);
+	set(GPS_navPoint2, 350, 220);
+	set(GPS_navPoint3, 350, 150);
+	set(GPS_navPoint4, 291, 90);
+
+	set(GPS_defendPointRamp, 80, -10);
+	set(GPS_prepareForKickstandReverse, 65, -35);
+	set(GPS_hitKickstandReverse, -10, -35);
 }
